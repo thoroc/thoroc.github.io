35b351601b1cee09f8d881596bb27d7fede1a348
thoroc <thomas.a.roche@gmail.com>
2014-04-09 14:10:58 +0100

    still mocking about

--- js/functions.js
+++ js/functions.js
@@ -9,26 +9,26 @@
 
 
 // remap jQuery to $
-(function($){
+( function( $ ) {
 
-	/* trigger when page is ready */
-	$(document).ready(function (){
-	
-		// your functions go here
-	
-	});
-	
-	
-	/* optional triggers
-	
-	$(window).load(function() {
-		
-	});
-	
-	$(window).resize(function() {
-		
-	});
-	
-	*/
+    /* trigger when page is ready */
+    $( document ).ready( function() {
 
-})(window.jQuery);
+        // your functions go here
+
+    });
+
+
+    /* optional triggers
+
+     $(window).load(function() {
+
+     });
+
+     $(window).resize(function() {
+
+     });
+
+     */
+
+})( window.jQuery );
